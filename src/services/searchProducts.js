b7aeab9be73142c63004a5da2816a96ced6e9d2d
juancgalueweb <juancgalue@icloud.com>
2023-03-13 16:49:15 -0300

    Agrega unos console.log para ver si se conecta con la API

--- src/services/searchProducts.js
+++ src/services/searchProducts.js
@@ -3,7 +3,7 @@ export const searchProducts = async () => {
     const products = await fetch('https://fakestoreapi.com/products').then(
       res => res.json()
     )
-
+    console.log('Búsqueda de productos realizada con éxito')
     return products
   } catch (error) {
     throw new Error('Error al buscar los productos en la API de fakestoreapi')
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/react-vite-shopping-cart/'
+  plugins: [react()]
 })
