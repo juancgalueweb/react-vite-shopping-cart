84e9a10ba9592117749cc305b183cb5b4e5cbdd4
juancgalueweb <juancgalue@icloud.com>
2023-03-13 16:54:19 -0300

    console.logs removidos. Despliege satisfactorio 🎉

--- src/services/categories.js
+++ src/services/categories.js
@@ -3,7 +3,6 @@ export const categories = async () => {
     const categories = await fetch(
       'https://fakestoreapi.com/products/categories'
     ).then(res => res.json())
-    console.log('Búsqueda de categorías realizada con éxito')
     return categories
   } catch (error) {
     throw new Error('Error al buscar las categorías en la API de fakestoreapi')
